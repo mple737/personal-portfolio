32be645cfec01af6ccd7ed129db2427ae5e1d770
ArsenalBAck2004 <utphuongmap@gmail.com>
2024-07-26 13:39:02 -0400

    "final commit"

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission
+        
+        var form = this;
+        var formData = new FormData(form);
+        
+        fetch(form.action, {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            if (response.ok) {
+                document.getElementById('submitSuccessMessage').classList.remove('d-none');
+                form.reset(); // Clear the form fields
+                setTimeout(() => {
+                    document.getElementById('submitSuccessMessage').classList.add('d-none');
+                }, 10000); // Hide success message after 5 seconds
+            } else {
+                document.getElementById('submitErrorMessage').classList.remove('d-none');
+                setTimeout(() => {
+                    document.getElementById('submitErrorMessage').classList.add('d-none');
+                }, 10000); // Hide error message after 5 seconds
+            }
+        }).catch(error => {
+            console.error('Form submission error:', error);
+            document.getElementById('submitErrorMessage').classList.remove('d-none');
+            setTimeout(() => {
+                document.getElementById('submitErrorMessage').classList.add('d-none');
+            }, 10000); // Hide error message after 5 seconds
+        });
+    });
+});
